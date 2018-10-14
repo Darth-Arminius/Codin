ba44487ae9f1daf3204d90b78701d1288478abaa
Darth-Arminius <arminpudic@yahoo.co.uk>
2018-10-14 22:43:03 +0100

    Add routing for home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
 import { MaterialModule } from "./material.module";
 
-const appRoutes: Routes = [{ path: "", component: AppComponent }];
+const appRoutes: Routes = [{ path: "home", component: HomeComponent }];
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
-    RouterModule.forRoot(appRoutes, { enableTracing: true }),
     BrowserModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
